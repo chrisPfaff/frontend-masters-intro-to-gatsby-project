d898f19d7515916c32ef4b0cf995ad09e53a64bb
Chris Pfaff <chrispfaff10@gmail.com>
2022-07-06 09:00:06 -0500

    config

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    sitUrl: 'https://www.blah.com',
+    title: 'Intro to gatsby',
+    description: 'Intro description',
+    image:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.AUZ4QVD-mj_nIOZ5QjFRuwHaF-%26pid%3DApi&f=1',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+};
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,9 +1,26 @@
 import * as React from 'react';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
 export default function IndexPage() {
-	return (
-		<main>
-			<h1>h1</h1>	
-		</main>
-	)
-};
+  const data = useStaticQuery(graphql`
+    query getSiteTitle {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+  const meta = data?.site?.siteMetadata ?? {};
+  return (
+    <>
+      <header>
+        <Link to="/">{meta.title}</Link>
+      </header>
+      <main>
+        <h1>h1</h1>
+        <Link to="/about">About</Link>
+      </main>
+    </>
+  );
+}
