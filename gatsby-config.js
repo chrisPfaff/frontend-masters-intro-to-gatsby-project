611ad34f246395852f51a25226984260577247fd
Chris Pfaff <chrispfaff10@gmail.com>
2022-07-06 19:26:16 -0500

    added seo for blogpost

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,32 +6,33 @@ module.exports = {
     image:
       'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.AUZ4QVD-mj_nIOZ5QjFRuwHaF-%26pid%3DApi&f=1',
   },
-    plugins: [
-      //PLUGIN FOR HEAD META TAGS
-      'gatsby-plugin-react-helmet',
-      //END PLUGIN FOR HEAD META TAGS
+  plugins: [
+    //PLUGIN FOR HEAD META TAGS
+    'gatsby-plugin-react-helmet',
+    //END PLUGIN FOR HEAD META TAGS
 
-      //THESE CONFIGS ARE FOR MDX MARKDOWN /POSTS AND ALLOWING THEM TO BE VIEWED AS PAGES
-     {
-       resolve: 'gatsby-source-filesystem',
-       options: {
-         name: 'posts',
-         path: `${__dirname}/src/posts/`,       },
-     },
-     {
-       resolve: 'gatsby-plugin-page-creator',
-       options: {
-         path: `${__dirname}/src/posts/`,       },
+    //THESE CONFIGS ARE FOR MDX MARKDOWN /POSTS AND ALLOWING THEM TO BE VIEWED AS PAGES
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/src/posts/`,
+      },
     },
-     {
-       resolve: 'gatsby-plugin-mdx',
-       options: {
-         defaultLayouts: {
-           posts: require.resolve('./src/components/layout.js'),
-         },
-       },
-     },
-     //END MDX CONFIG FOR POSTS
-
-    ],
-  };
+    {
+      resolve: 'gatsby-plugin-page-creator',
+      options: {
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          posts: require.resolve('./src/components/postlayout.js'),
+        },
+      },
+    },
+    //END MDX CONFIG FOR POSTS
+  ],
+};
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,12 +1,40 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
-import Layout  from "../components/layout.js"
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import Layout from '../components/layout.js';
 
 export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query GetBlogPosts {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        nodes {
+          id
+          slug
+          frontmatter {
+            description
+            date(fromNow: true)
+            title
+          }
+        }
+      }
+    }
+  `);
+  const posts = data.allMdx.nodes;
+
   return (
     <Layout>
-        <h1>h1</h1>
-        <Link to="https://www.google.com" target="_blank">Google</Link>
+      <h1>h1</h1>
+      <Link to="https://www.google.com" target="_blank">
+        Google
+      </Link>
+      <h2>Check out the blog</h2>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={post.slug}>{post.frontmatter.title} </Link>{' '}
+            <small>posted {post.frontmatter.date}</small>
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 }
